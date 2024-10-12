396c658a42bf165ecda9bb8b2fc57c023c21c9df
pepesan <pepesan@gmail.com>
2024-10-12 21:18:35 +0200

    metido ejemplo de variables de boolean y enteros (int)

---
+++ variables.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+    print("Ejemplos de variables")
+    # Declaración e inicilización de variable
+    # boolean: True, False, None
+    booleana = True
+    # imprimir por pantalla variable
+    print(booleana)
+    # Declaración de variable entera
+    # enteros: numero naturales, positivos o negativos
+    entero = 2
+    # imprimir por pantalla variable
+    print(entero)
+    # cambiamos el valor de una variable
+    entero = 3
+    # imprimir por pantalla variable
+    print(entero)
+    # Definimos otra variable entera
+    numero = 2
+    # imprimir por pantalla variable
+    print(numero)
