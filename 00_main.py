ee02cd103bd4d7bbb87024d7a7c684c0540d6fca
pepesan <pepesan@gmail.com>
2024-11-09 20:30:14 +0100

    cambiados nombre de los ejemplos para que sean más facilmente encontrables y metido ejemplo de operadores aritméticos y booleanos

--- 00_main.py
+++ 00_main.py
@@ -1,4 +1,4 @@
-# main.py
+# 00_main.py
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('Hola Mundo')
---
+++ 03_operadores.py
@@ -0,0 +1,36 @@
+if __name__ == '__main__':
+    #Operadores aritméticos
+    # Suma
+    print("Suma")
+    print(2 + 4)
+    # Resta
+    print("Resta")
+    print(6 - 4)
+    # Multiplicación
+    print("Multiplicación")
+    print(6 * 3)
+    # División Flotante
+    print("División Flotante")
+    print(6 / 3)
+    # División Entera
+    print("División Entera")
+    print("6 // 3")
+    print(6 // 3)  # floor (integer) division: always truncates fractional remainders
+    print("7 // 3")
+    print(7 // 3)  # floor (integer) division: always truncates fractional remainders
+    # Módulo
+    print("Módulo resultado 6/3 = 2 (resto 0)")
+    print(6 % 3)
+    print("Módulo resultado 6/4 = 1 (resto 2)")
+    print(6 % 4)
+    # Potencia
+    print("Potencia")
+    print(3 ** 2)  # 3 elevado a 2 = 9
+
+    # operadores Booleanos
+    # operación AND ambos deben ser True para devolver True
+    print(True and False)
+    # operación OR Uno de ellos debe ser True para devolver True
+    print(True or False)
+    # operación Not cambia el valor True->False y False->True
+    print(not True)
